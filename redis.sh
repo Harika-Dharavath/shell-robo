84dcf358b431d406099856d754d4e8f1ed24f518
harika dharavath <harika@joindevops.com>
2025-11-22 10:34:15 +0530

    open

--- redis.sh
+++ redis.sh
@@ -40,7 +40,7 @@ VALIDATECOMMAND $? "Enabling Redis 7 module"
 dnf install redis -y &>>$LOG_FILE
 VALIDATECOMMAND $? "Redis"
 
-sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no /etc/redis/redis.conf'
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no ' /etc/redis/redis.conf &>>$LOG_FILE
 VALIDATECOMMAND $? "allowing remote connections in redis.conf"
 
 systemctl enable redis &>>$LOG_FILE
@@ -48,3 +48,6 @@ VALIDATECOMMAND $? "Enabling Redis service"
 systemctl start redis &>>$LOG_FILE
 VALIDATECOMMAND $? "Starting Redis service"
 
+END_TIME=$(date +%s)
+TOTAL_TIME=$(($END_TIME-$START_TIME))
+echo -e "$G Script executed successfully in $TOTAL_TIME seconds. $N" | tee -a $LOG_FILE
