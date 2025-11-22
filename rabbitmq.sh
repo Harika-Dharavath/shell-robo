ed2627dcc09262d00d5e7f598858ca8374085e2d
harika dharavath <harika@joindevops.com>
2025-11-22 20:26:19 +0530

    open

---
+++ rabbitmq.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+R="\e[31m" # Red color
+G="\e[32m" # Green color
+Y="\e[0;33m" # Yellow color]
+B="\e[1;33M" # Bold Yellow color
+O="\e[1;34m" # Bold Blue color
+N="\e[0m"  # No Color
+
+LOGS_FOLDER="/var/log/shell-roboshop"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1) #. tarvatha vache daani print cheyadu 
+SCRIPT_DIR=$PWD
+MONGODB_HOST=mongodb.daw86s.space
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOGS_FOLDER
+echo -e "$G script started executed at : $(date) $N" | tee -a $LOG_FILE #tee lets you see the output on the screen while also saving it to a file.
+
+USERID=$(id -u) # prints the user id of current user
+if [ $USERID -ne 0 ]; then
+   echo  -e " $R You must run this script as root user."  #i forgot ot add -e check it 
+   exit 1 #other than 0 take it as failure ''
+fi
+
+VALIDATECOMMAND(){ #no space should be between validate command and ()
+    if [ $1 -ne 0 ]; then
+        echo -e "$B .... $R Error: $2 installation failed."| tee -a $LOG_FILE
+        #ACCORDING to our present code $2 is mysql ,mgodb,ngnix
+        exit 1
+        
+
+    else 
+        echo -e "$O $2.....$G sucessfully. $N"| tee -a $LOG_FILE
+
+    fi
+}        
+
+cp $SCRIPT_DIR/rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo &>>$LOG_FILE
+VALIDATECOMMAND $? "Adding RabbitMQ repository"
+dnf install rabbitmq-server -y &>>$LOG_FILE
+VALIDATECOMMAND $? "RabbitMQ Server"
+systemctl enable rabbitmq-server &>>$LOG_FILE
+VALIDATECOMMAND $? "Enabling RabbitMQ service"
+systemctl start rabbitmq-server &>>$LOG_FILE
+VALIDATECOMMAND $? "Starting RabbitMQ service"
+
+rabbitmqctl add_user roboshop Rabbit@123 &>>$LOG_FILE
+VALIDATECOMMAND $? "Adding RabbitMQ user"
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>$LOG_FILE
+
+END_TIME=$(date +%s)
+TOTAL_TIME=$(($END_TIME-$START_TIME))
+echo -e "$G Script executed successfully in $TOTAL_TIME seconds. $N" | tee -a $LOG_FILE
