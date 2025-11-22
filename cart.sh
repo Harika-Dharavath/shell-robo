cc8fb11414b3e864fbb43b779a2c99a8b44e5470
harika dharavath <harika@joindevops.com>
2025-11-22 21:25:41 +0530

    open

---
+++ cart.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+R="\e[31m" # Red color
+G="\e[32m" # Green color
+Y="\e[0;33m" # Yellow color]
+B="\e[1;33M" # Bold Yellow color
+O="\e[1;34m" # Bold Blue color
+N="\e[0m"  # No Color
+
+LOGS_FOLDER="/var/log/shell-roboshop"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1) #. tarvatha vache daani print cheyadu 
+SCRIPT_DIR=$PWD
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+
+mkdir -p $LOGS_FOLDER
+echo -e "$G script started executed at : $(date) $N" | tee -a $LOG_FILE #tee lets you see the output on the screen while also saving it to a file.
+
+USERID=$(id -u) # prints the user id of current user
+if [ $USERID -ne 0 ]; then
+   echo  -e " $R You must run this script as root user."  #i forgot ot add -e check it 
+   exit 1 #other than 0 take it as failure ''
+fi
+
+VALIDATECOMMAND(){ #no space should be between validate command and ()
+    if [ $1 -ne 0 ]; then
+        echo -e "$B .... $R Error: $2 installation failed."| tee -a $LOG_FILE
+        #ACCORDING to our present code $2 is mysql ,mgodb,ngnix
+        exit 1
+        
+
+    else 
+        echo -e "$O $2.....$G sucessfully. $N"| tee -a $LOG_FILE
+
+    fi
+}      
+
+dnf module disable nodejs -y &>>$LOG_FILE
+VALIDATECOMMAND $? "Disabling Nodejs module"
+
+dnf module enable nodejs:20 -y &>>$LOG_FILE
+VALIDATECOMMAND $? "Enabling Nodejs 20 module"
+
+dnf install nodejs -y &>>$LOG_FILE
+VALIDATECOMMAND $? "Nodejs"
+
+id roboshop &>>$LOG_FILE
+if [ $? -ne 0 ]; then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
+    VALIDATECOMMAND $? "Creating roboshop user"
+else
+        echo -e "$O roboshop user already exists. Skipping user creation. $N" &>>$LOG_FILE
+ fi
+
+mkdir -p /app 
+VALIDATECOMMAND $? "Creating /app directory"
+
+curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart-v3.zip &>>$LOG_FILE
+VALIDATECOMMAND $? "Downloading cart component"
+
+cd /app
+VALIDATECOMMAND $? "Changing directory to /app"
+
+rm -rf /app/*
+VALIDATECOMMAND $? "Cleaning up old catalogue content" 
+
+unzip /tmp/user.zip &>>$LOG_FILE
+VALIDATECOMMAND $? "Extracting catalogue component"
+
+npm install &>>$LOG_FILE
+VALIDATECOMMAND $? "Installing nodejs dependencies for catalogue"
+
+
+cp $SCRIPT_DIR/cart.service /etc/systemd/system/user.service &>>$LOG_FILE
+VALIDATECOMMAND $? "Copying user service file"
+
+systemctl daemon-reload &>>$LOG_FILE
+VALIDATECOMMAND $? "Reloading systemd daemon"
+systemctl enable user &>>$LOG_FILE
+VALIDATECOMMAND $? "Enabling user service"
+systemctl start user &>>$LOG_FILE
+VALIDATECOMMAND $? "Starting user service"
