d6e1c4a03ea3ac20c1b98a49dd22181042f2bec1
harika dharavath <harika@joindevops.com>
2025-11-25 15:05:50 +0530

    message

--- redis.sh
+++ redis.sh
@@ -1,36 +1,6 @@
-# Red color
-G="\e[32m" # Green color
-Y="\e[0;33m" # Yellow color]
-B="\e[1;33M" # Bold Yellow color
-O="\e[1;34m" # Bold Blue color
-N="\e[0m"  # No Color
+#!/bin/bash
 
-LOGS_FOLDER="/var/log/shell-roboshop"
-SCRIPT_NAME=$( echo $0 | cut -d "." -f1) #. tarvatha vache daani print cheyadu 
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
-START_TIME=$(date +%s)
-
-mkdir -p $LOGS_FOLDER
-echo -e "$G script started executed at : $(date) $N" | tee -a $LOG_FILE #tee lets you see the output on the screen while also saving it to a file.
-
-USERID=$(id -u) # prints the user id of current user
-if [ $USERID -ne 0 ]; then
-   echo  -e " $R You must run this script as root user."  #i forgot ot add -e check it 
-   exit 1 #other than 0 take it as failure ''
-fi
-
-VALIDATECOMMAND(){ #no space should be between validate command and ()
-    if [ $1 -ne 0 ]; then
-        echo -e "$B .... $R Error: $2 installation failed."| tee -a $LOG_FILE
-        #ACCORDING to our present code $2 is mysql ,mgodb,ngnix
-        exit 1
-        
-
-    else 
-        echo -e "$O $2.....$G sucessfully. $N"| tee -a $LOG_FILE
-
-    fi
-}    
+source ./common.sh    
 
 #installing redis  
 
